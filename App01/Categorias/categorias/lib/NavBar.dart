6c495effa577bc2bfa58a13784704dbda17ce888
daniela5Ramos <114941988+daniela5Ramos@users.noreply.github.com>
2022-11-05 12:15:02 -0600

    dos

--- App01/Categorias/categorias/lib/NavBar.dart
+++ App01/Categorias/categorias/lib/NavBar.dart
@@ -28,7 +28,7 @@ class NavBar extends StatelessWidget {
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: NetworkImage(
-                  '',
+                  'file:///C:/Users/Erika%20Ramirez/Documents/T202_FlutterApps/imagenes/web_first_images_release_0.png',
                 ),
                 fit: BoxFit.cover,
               ),
