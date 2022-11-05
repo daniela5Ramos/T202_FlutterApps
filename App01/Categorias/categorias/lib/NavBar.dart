a86c38367a94e0a9258f7415847333a98f72c254
daniela5Ramos <114941988+daniela5Ramos@users.noreply.github.com>
2022-11-05 11:57:03 -0600

    dos

--- App01/Categorias/categorias/lib/NavBar.dart
+++ App01/Categorias/categorias/lib/NavBar.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
@@ -7,6 +9,21 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('DanielaRamis'),
+            accountEmail: Text('rmsyazmin185@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.network(
+                    'file:///C:/Users/Erika%20Ramirez/Documents/METRICAS/depositphotos_69189435-stock-photo-funny-monkey-with-a-red.jpg'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
